ab2f49f8d1ffa3be2dcef81fa7d05a9f96b8ecd5
arpitkothiyasnowflake <137180800+arpitkothiyasnowflake@users.noreply.github.com>
2023-06-24 17:19:46 +0530

    Update streamlit_app.py
    error resolved

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-stearmlit.text('🥑🍞 Avacado Toast'
+stearmlit.text('🥑🍞 Avacado Toast')
